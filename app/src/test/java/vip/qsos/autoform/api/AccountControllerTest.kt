328a3cbb901f364a87c809486476d99da10aa528
华清松 <821034742@qq.com>
2020-09-26 00:33:42 +0800

    调试成功

--- app/src/test/java/vip/qsos/autoform/api/AccountControllerTest.kt
+++ app/src/test/java/vip/qsos/autoform/api/AccountControllerTest.kt
@@ -1,6 +1,5 @@
 package vip.qsos.autoform.api
 
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
@@ -11,13 +10,14 @@ import vip.qsos.autoform.data_jpa.repository.TableFormRepository
 import vip.qsos.autoform.data_jpa.repository.TableFormValueRepository
 import vip.qsos.autoform.utils.FormDemoUtils
 import javax.annotation.Resource
+import javax.transaction.Transactional
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         classes = [ServerLauncher::class]
 )
-class TestRedisUtil {
+open class TestRedisUtil {
 
     @Resource
     private lateinit var mTableFormRepository: TableFormRepository
@@ -28,35 +28,20 @@ class TestRedisUtil {
     @Resource
     private lateinit var mTableFormValueRepository: TableFormValueRepository
 
-    private var formId: Long = -1L
-
-    @Before
-    fun before() {
-        mTableFormRepository.deleteAll()
-    }
-
     @Test
-    fun create() {
+    @Transactional
+    open fun create() {
         val form = FormDemoUtils.Create.feedbackForm()
-        formId = mTableFormRepository.save(form).id
-        println("创建完成，ID=$formId")
-        form.formItems.forEach {
-            mTableFormItemRepository.save(it)
-            mTableFormValueRepository.saveAll(it.formValues)
+        mTableFormRepository.save(form).apply {
+            println("表单创建完成，表单-${this.title}，ID=${this.id}")
+            this.formItems.forEach {
+                println("表单创建完成，表单项-${it.title}，ID=${it.id}")
+                it.formValues.forEach { v ->
+                    println("表单创建完成，表单项值-${it.title}，ID=${v.id}")
+                }
+            }
         }
+
     }
 
-    @Test
-    fun find() {
-        val form = mTableFormRepository.findById(formId).get()
-        println(form.toString())
-    }
-
-    @Test
-    fun findAll() {
-        val r = mTableFormRepository.findAll()
-        r.forEach {
-            println(it.toString())
-        }
-    }
 }
